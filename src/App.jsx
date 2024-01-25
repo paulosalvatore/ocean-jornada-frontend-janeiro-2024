47a8d6467750ad0130457b3e4fd8dd6ea98a60a8
Paulo Salvatore <salvatoregames@gmail.com>
2024-01-25 15:00:19 +0000

    Extração do componente `<Card/ >`

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
   return (
     <>
-      <div className="card">
-        <h2>Rick Sanchez</h2>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-      </div>
+      <Card />
+      <Card />
     </>
   )
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,12 @@
+function Card() {
+  return (
+    <>
+      <div className="card">
+        <h2>Rick Sanchez</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+      </div>
+    </>
+  )
+}
+
+export default Card
