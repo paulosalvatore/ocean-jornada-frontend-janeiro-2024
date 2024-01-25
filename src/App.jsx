5c408c72c21fa22caf943b725ffd197a6d0e263b
Paulo Salvatore <salvatoregames@gmail.com>
2024-01-25 14:02:23 +0000

    Borda adicionada

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <div>
+      <div className="card">
         <h2>Rick Sanchez</h2>
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
       </div>
