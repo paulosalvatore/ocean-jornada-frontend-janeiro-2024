83c0a158c0eec04925b5ea720491b1b9c4971db8
Paulo Salvatore <salvatoregames@gmail.com>
2024-01-30 13:40:41 +0000

    Criação de novos Cards com outros valores

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,21 @@ function App() {
     image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
   }
 
+  const item2 = {
+    name: 'Morty Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+
+  const item3 = {
+    name: 'Summer Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  }
+
   return (
     <>
       <Card item={item1} />
-      <Card item={item1} />
+      <Card item={item2} />
+      <Card item={item3} />
     </>
   )
 }
