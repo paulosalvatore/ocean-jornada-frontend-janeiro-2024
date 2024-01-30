294ea6caaa8efda9e7d79970be95962341598dc3
Paulo Salvatore <salvatoregames@gmail.com>
2024-01-30 13:36:26 +0000

    Configuração da props para o Card

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
+  const item1 = {
+    name: 'Rick Sanchez',
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  }
+
   return (
     <>
-      <Card />
-      <Card />
+      <Card item={item1} />
+      <Card item={item1} />
     </>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,14 @@
-function Card() {
+function Card(props) {
+  //console.log(props)
+
+  const item = props.item
+
+  //console.log(item)
+
   return (
     <>
       <div className="card">
-        <h2>Rick Sanchez</h2>
+        <h2>{item.name}</h2>
 
         <div className="tags">
           <div className="tag">Status: Vivo</div>
@@ -10,7 +16,7 @@ function Card() {
           <div className="tag">Origem: Terra C-137</div>
         </div>
 
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+        <img src={item.image} />
       </div>
     </>
   )
