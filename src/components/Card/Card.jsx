1d43bfceadaa8673a18dcd815227c578f68a79ff
Paulo Salvatore <salvatoregames@gmail.com>
2024-01-30 12:49:39 +0000

    Criação e estilização das tags

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,6 +3,13 @@ function Card() {
     <>
       <div className="card">
         <h2>Rick Sanchez</h2>
+
+        <div className="tags">
+          <div className="tag">Status: Vivo</div>
+          <div className="tag">Espécie: Humana</div>
+          <div className="tag">Origem: Terra C-137</div>
+        </div>
+
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
       </div>
     </>
