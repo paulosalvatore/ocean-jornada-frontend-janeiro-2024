a88952c03bef4640471b55d52dd7cca63f275c9b
Paulo Salvatore <salvatoregames@gmail.com>
2024-01-31 14:38:38 +0000

    Criação do estado `itens` (WIP)

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
+import { useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
   // Carregamento de dados direto via código
-  const item1 = {
-    name: 'Rick Sanchez',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
-  }
+  // const item1 = {
+  //   name: 'Rick Sanchez',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  // }
 
-  const item2 = {
-    name: 'Morty Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
-  }
+  // const item2 = {
+  //   name: 'Morty Smith',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  // }
 
-  const item3 = {
-    name: 'Summer Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
-  }
+  // const item3 = {
+  //   name: 'Summer Smith',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  // }
 
-  const item4 = {
-    name: 'Beth Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
-  }
+  // const item4 = {
+  //   name: 'Beth Smith',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
+  // }
 
-  const itens = [item1, item2, item3, item4]
+  // const itens = [item1, item2, item3, item4]
+
+  const [itens, setItens] = useState([])
 
   // Carregamento de dados via API (Backend)
   async function carregarDadosApi() {
@@ -37,11 +40,16 @@ function App() {
 
     const body = await response.json()
 
-    console.log(body)
+    //console.log(body)
 
     // Extrair a propriedade results do body
     // Essa propriedade contém a lista de itens
-    const results = body.results;
+    const results = body.results
+
+    console.log(results)
+
+    // Atualiza o estado "itens" com os resultados da API
+    setItens(results)
 
     // TODO: Desafio p/ volta do intervalo:
     // Enviar os "results" para o JSX, exibindo
