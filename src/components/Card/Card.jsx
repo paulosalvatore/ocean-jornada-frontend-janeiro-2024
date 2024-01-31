b755193c6b8390b64ccdde0b8ba0bdd5a7b8662c
Paulo Salvatore <salvatoregames@gmail.com>
2024-01-31 15:05:38 +0000

    (Bônus) Correção da responsividade dos Cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -10,13 +10,15 @@ function Card(props) {
       <div className="card">
         <h2>{item.name}</h2>
 
-        <div className="tags">
-          <div className="tag">Status: Vivo</div>
-          <div className="tag">Espécie: Humana</div>
-          <div className="tag">Origem: Terra C-137</div>
-        </div>
+        <div>
+          <div className="tags">
+            <div className="tag">Status: Vivo</div>
+            <div className="tag">Espécie: Humana</div>
+            <div className="tag">Origem: Terra C-137</div>
+          </div>
 
-        <img src={item.image} />
+          <img src={item.image} />
+        </div>
       </div>
     </>
   )
